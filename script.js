218cda00cb42a4b04658a3a50b7a0aee329c2d27
Erwan LE PIERRES <55241151+Erwan33800@users.noreply.github.com>
2021-03-02 16:42:48 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+var animation = document.getElementById('animation-text'),
+    list = [
+    'HTML',
+    'CSS',
+    'PHP',
+    'Javascript',
+    'React',
+],
+    count_li = 0,
+    count = 0,
+    speed = 100;
+
+function write() {
+    var type = list[count_li].substring(0, count);
+    animation.textContent = type;
+    count++;
+    var time = setTimeout(write, speed);
+    if(count > list[count_li].length) {
+        count = 0;
+        count_li++;
+        clearTimeout(time);
+        setTimeout(write, 2500);
+    }
+    if(count_li === list.length) {
+        count_li = 0;
+    }
+}
+write();
